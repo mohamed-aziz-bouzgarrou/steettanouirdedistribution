2c2e2bac5418eb26509c4f7ff08bb026f1d4bb15
Mohamed Aziz Bouzgarrou <109080661+mohamed-aziz-bouzgarrou@users.noreply.github.com>
2026-08-21 08:46:58 +0100

    tttt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-      <Routes>
-            <Route path="/" element={<h1>HOME ROUTE</h1>} />
-                  <Route path="/products" element={<h1>PRODUCTS ROUTE</h1>} />
-                        <Route path="*" element={<h1>NOT FOUND ROUTE</h1>} />
-                            </Routes>
-                              );
-                              }
+      <div className='min-h-screen flex flex-col font-sans text-gray-800'>
+            <Navbar />
+                  <main className='flex-1'>
+                          <Routes>
+                                    <Route path='/' element={<Home />} />
+                                              <Route path='/products' element={<Products />} />
+                                                        <Route path='/products/:id' element={<ProductDetail />} />
+                                                                  <Route path='*' element={<NotFound />} />
+                                                                          </Routes>
+                                                                                </main>
+                                                                                      <Footer />
+                                                                                          </div>
+                                                                                            );
+                                                                                            }
